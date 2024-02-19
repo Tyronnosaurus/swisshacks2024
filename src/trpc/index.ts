5d4cfb8a6a0745eebf96d64c74ab5cdcd252bb5f
Tyronnosaurus <Tyronnosaurus@protonmail.com>
2024-02-19 22:49:41 +0100

    Connected database

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -1,18 +1,34 @@
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
 import { publicProcedure, router } from './trpc';
 import { TRPCError } from '@trpc/server';
+import { db } from '@/db';
  
 // queries are for GET requests
 // mutations are for POST, UPDATE... requests
 
 export const appRouter = router({
-  authCallback: publicProcedure.query(() => {
+  authCallback: publicProcedure.query(async() => {
     const {getUser} = getKindeServerSession()
     const user = getUser()
 
     if (!user.id || !user.email) throw new TRPCError({code: "UNAUTHORIZED"})
 
-    // Check if the user is in the database
+    // Check if the user that is logged in is also in the database
+    const dbUser = await db.user.findFirst({
+      where: {
+        id: user.id
+      }
+    })
+
+    if (!dbUser) {
+      // Create user in db
+      await db.user.create({
+        data: {
+          id: user.id,
+          email: user.email
+        }
+      })
+    }
 
     return({success: true})
   })
