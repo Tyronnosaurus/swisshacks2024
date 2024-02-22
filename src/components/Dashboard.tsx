d5b5a52a4681faa4074b6051dd8c2fd36d9046ae
Tyronnosaurus <Tyronnosaurus@protonmail.com>
2024-02-22 19:17:17 +0100

    Show day on the upload date

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -37,6 +37,7 @@ const Dashboard = () => {
           {files.sort(
             (a,b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime())
             .map((file) => (
+
               <li key={file.id} className="col-span-1 divide-y divide-gray-200 rounded-lg bg-white shadow transition hover:shadow-lg">
                 <Link href={`/dashboard/${file.id}`} className="flex flex-col gap-2">
                   <div className="pt-6 px-6 flex w-full items-cneter justify-between space-x-6">
@@ -57,7 +58,7 @@ const Dashboard = () => {
                 <div className="px-6 mt-4 grid grid-cols-3 place-items-center py-2 gap-6 text-xs text-zinc-500">
                   <div className="flex items-center gap-2">
                     <Plus className="h-4 w-4" />
-                    {format(new Date(file.createdAt), "MMM yyyy")}
+                    {format(new Date(file.createdAt), "dd MMM yyyy")}
                   </div>
 
                   <div className="flex items-center gap-2">
@@ -71,8 +72,9 @@ const Dashboard = () => {
                   </Button>
                 </div>
               </li>
-            ))
-          }
+
+            )
+          )}
         </ul>
       ) : isLoading ? (
         <Skeleton height={100} className="my-2" count={3} />
