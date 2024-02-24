38a00184f62b715fbf8dd338203bc0efce6c97b1
Tyronnosaurus <Tyronnosaurus@protonmail.com>
2024-02-24 15:08:24 +0100

    Refactoring

--- src/components/UploadDropZone.tsx
+++ src/components/UploadDropZone.tsx
@@ -78,7 +78,6 @@ const UploadDropZone = () => {
                                 }))
                             }
 
-
                             clearInterval(progressInterval)
                             setUploadProgress(100)
 
@@ -89,6 +88,8 @@ const UploadDropZone = () => {
                     <div className="flex items-center justify-center h-full w-full">
                         <label htmlFor="dropzone-file"
                                className="flex flex-col items-center justify-center w-full h-full rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100">
+                                
+                                {/* Cloud icon, instructions, and text stating the max file size */}
                                 <div className="flex flex-col items-center justify-center pt-5 pb-6">
                                     <Cloud className="h-6 w-6 text-zinc-500 mb-2" />
                                     <p className="mb-2 text-sm text-zinc-700">
@@ -99,7 +100,8 @@ const UploadDropZone = () => {
                                     </p>
                                 </div>
 
-                                {acceptedFiles && acceptedFiles[0] ? (
+                                {/* File icon and filename */}
+                                {(acceptedFiles && acceptedFiles[0]) && (
                                     <div className="max-w-xs bg-white flex items-center rounded-md overflow-hidden outline outline-[1px] outline-zinc-200 divide-x divide-zinc-200">
                                         <div className="px-3 py-2 h-full grid place-items-center">
                                             <File className="h-4 w-4 text-blue-500" />
@@ -108,12 +110,15 @@ const UploadDropZone = () => {
                                             {acceptedFiles[0].name}
                                         </div>
                                     </div>
-                                ) : null}
+                                )}
 
-                                {isUploading ? (
+                                {/* Progress bar */}
+                                {isUploading && (
                                     <div className="w-full mt-4 max-w-xs mx-auto">
                                         <Progress value={uploadProgress} className="h-1 w-full bg-zinc-200" />
                                     </div>
+                                )}
+
                                 {/* Show a "redirecting" text when upload finishes */}
                                 {uploadProgress===100 && (
                                     <div className="flex gap-1 items-center justify-center text-sm text-zinc-700 pt-2">
