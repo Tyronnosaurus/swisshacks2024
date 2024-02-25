64c57a1fa45494d4b608b51ffe2fdc7fef8c4aed
Tyronnosaurus <Tyronnosaurus@protonmail.com>
2024-02-25 19:42:43 +0100

    Added loading icon and error toast to the PDF renderer

--- src/components/PdfRenderer.tsx
+++ src/components/PdfRenderer.tsx
@@ -1,8 +1,10 @@
 "use client"
 
+import { Loader2 } from "lucide-react";
 import {Document, Page, pdfjs} from "react-pdf"
 import 'react-pdf/dist/Page/AnnotationLayer.css'; // Support for annotations
 import 'react-pdf/dist/Page/TextLayer.css'; // Support for text layer (for text selection & search)
+import { useToast } from "./ui/use-toast";
 
 // Worker. Necessary for rendering PDFs
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`
@@ -19,6 +21,8 @@ interface PdfRendererProps {
 
 const PdfRenderer = ({url}:PdfRendererProps) => {
 
+  const {toast} = useToast()
+  
   return (
     <div className="w-full bg-white rounded-md shadow flex flex-col items-center">
 
@@ -32,7 +36,20 @@ const PdfRenderer = ({url}:PdfRendererProps) => {
       {/* PDF rendering */}
       <div className="flex-1 w-full max-h-screen">
         <div>
-          <Document file={url} className="max-h-full">
+          <Document loading={
+                      <div className="flex justify-center">
+                        <Loader2 className="my-24 h-6 w-6 animate-spin" />
+                      </div>
+                    }
+                    onLoadError={() => {
+                      toast({
+                        title: "Error loading PDF",
+                        description: "Please try again later",
+                        variant: "destructive"
+                      })
+                    }}
+                    file={url}
+                    className="max-h-full">
             <Page pageIndex={0}/>
           </Document>
         </div>
