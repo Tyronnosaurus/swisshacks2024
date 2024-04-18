dd37962d0cbad75b9139fc1f4517e7001bf5d24c
Eduard Tiron <tyronnosaurus@protonmail.com>
2024-04-18 23:49:27 +0200

    Moved chat components to chat folder. Show feedback if file upload not finished or failed. Created ChatInput and Messages placeholder components.

--- src/app/dashboard/[fileid]/page.tsx
+++ src/app/dashboard/[fileid]/page.tsx
@@ -1,4 +1,4 @@
-import ChatWrapper from "@/components/ChatWrapper"
+import ChatWrapper from "@/components/chat/ChatWrapper"
 import PdfRenderer from "@/components/PdfRenderer"
 import { db } from "@/db"
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server"
@@ -34,14 +34,14 @@ const Page = async ({params}: PageProps) => {
     return (
         <div className="flex-1 justify-between flex flex-col h-[calc(100vh-3.5rem)]">
             <div className="mx-auto w-full max-w-8xl frow lg:flex lg:px-2">
-                {/* Left side */}
+                {/* Left side (or top on small screens) */}
                 <div className="px-4 py-6 sm:px-6 lg:pl-8 xl:flex-1 xl:pl-6">
                     <PdfRenderer url={file.url}/>
                 </div>
 
-                {/* Right side */}
+                {/* Right side (or bottom on small screens) */}
                 <div className="shrink-0 flex-[0.75] border-t border-gray-200 lg:w-96 lg:border-l lg:border-t-0">
-                    <ChatWrapper />
+                    <ChatWrapper fileId={file.id}/>
                 </div>
             </div>
         </div>
---
+++ src/components/chat/ChatInput.tsx
@@ -0,0 +1,15 @@
+import { boolean } from "zod"
+
+interface ChatInputProps {
+  isDisabled?: boolean
+}
+
+const ChatInput = ({isDisabled}: ChatInputProps) => {
+  return (
+    <div>
+        ChatInput
+    </div>
+  )
+}
+
+export default ChatInput
