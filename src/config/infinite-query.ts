f9781d442ef5e2b415d4c296dc4327c33d490735
Eduard Tiron <tyronnosaurus@protonmail.com>
2024-05-04 10:57:22 +0200

    Added TRPC procedure to get previous chat messages, for infinite scrolling

---
+++ src/config/infinite-query.ts
@@ -0,0 +1,2 @@
+// How many messages to load when the page loads, and every time the user scrolls up to load more
+export const INFINITE_QUERY_LIMIT = 10
--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -3,6 +3,8 @@ import { privateProcedure, publicProcedure, router } from './trpc';
 import { TRPCError } from '@trpc/server';
 import { db } from '@/db';
 import { z } from 'zod'
+import { INFINITE_QUERY_LIMIT } from '@/config/infinite-query';
+
  
 // queries are for GET requests
 // mutations are for POST, UPDATE... requests
@@ -51,7 +53,60 @@ export const appRouter = router({
     })
   }),
 
-  // Get uploadStatus (PENDING, PROCESSING, FAILED OR SUCCESS)
+
+  // Private procedure to fetch previous chat messages for a specific file, in an infinite query way:
+  // we can pass a cursor, and it will fetch N msgs starting at the Mth position going back in time.
+  getFileMessages: privateProcedure
+    .input(
+      z.object({
+        limit: z.number().min(1).max(100).nullish(),  // Number of messages to get. Optional
+        cursor: z.string().nullish(),                 // Msg Id from which to start the batch. Optional, if not given we'll get the most recent msgs.
+        fileId: z.string()
+      })
+    )
+    .query(async({ctx, input}) => {
+      const {userId} = ctx 
+      const {fileId, cursor} = input
+      const limit = input.limit ?? INFINITE_QUERY_LIMIT // How many msgs to get per request. If not specified, a default value is used.
+
+      // Get file info from database
+      const file = await db.file.findFirst({
+        where: {
+          id: fileId,
+          userId
+        }
+      })
+
+      if(!file) throw new TRPCError({ code: 'NOT_FOUND' })
+
+      // Fetch msgs from database
+      const messages = await db.message.findMany({
+        take: limit + 1,  // Get one more than needed. We'll remove it but save its Id.
+        where: { fileId },
+        orderBy: { createdAt: "desc" },
+        cursor: cursor ? {id: cursor} : undefined,
+        select: {
+          id: true,
+          isUserMessage: true,
+          createdAt: true,
+          text: true
+        }
+      })
+
+      let nextCursor: typeof cursor | undefined = undefined
+
+      // If the db returned as many msgs as we requested, it means we haven't reached the oldest msg yet.
+      // Save the Id of the extra msg we added to the batch and remove it.
+      if(messages.length > limit) {
+        const nextItem = messages.pop()
+        nextCursor = nextItem?.id
+      }
+
+      return({messages, nextCursor})
+    }),
+
+
+  // Get uploadStatus (PENDING, PROCESSING, FAILED or SUCCESS)
   getFileUploadStatus: privateProcedure
     .input(z.object({fileId: z.string()}))
     .query(async ({input, ctx}) => {
