5a42464f137ceffd4fb81365b05d8d82ca927244
Eduard Tiron <tyronnosaurus@protonmail.com>
2024-05-11 18:57:08 +0200

    Implemented UpgradeButton functionality

--- src/components/UpgradeButton.tsx
+++ src/components/UpgradeButton.tsx
@@ -1,10 +1,26 @@
+"use client"
+
 import { ArrowRight } from "lucide-react"
 import { Button } from "./ui/button"
+import { trpc } from "@/app/_trpc/client"
 
+
+/** Fancy "Subscribe" button that appears on the Pricing page under the Pro plan, to catch the attention of the user */
 const UpgradeButton = () => {
+
+  // When button is clicked, we'll create a Stripe session. Doing so will
+  // return a url to go Stripe's checkout page
+  const {mutate: createStripeSession} = trpc.createStripeSession.useMutation({
+    onSuccess: ({url}) => {
+      window.location.href = url ?? "/dashboard/billing"  // Redirect user to checkout page (or stay on billing page if createStripeSession fails to return an url)
+    }
+  })
+
+
   return (
-    <Button className="w-full">
-        Upgrade now <ArrowRight className="h-5 w-5 ml-1.5" />
+    <Button onClick={() => createStripeSession()} className="w-full">
+        Upgrade now
+        <ArrowRight className="h-5 w-5 ml-1.5" />
     </Button>
   )
 }
