d0aeb6e747cdd7d29fe194f0fa83593e0e571481
Eduard Tiron <tyronnosaurus@protonmail.com>
2024-05-11 19:13:11 +0200

    Comments

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,12 +5,17 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-
+/** Generate absolute url from relative path */
 export function absoluteUrl(path: string) {
+  // When called from a browser, return url as is
   if (typeof window !== 'undefined') return(path)
   
+  // When executed server-side (e.g., in a Node.js environment),
+  // if the website has been deployed and is on Vercel, construct url accordingly.
+  // Vercel automatically creates the VERCEL_URL system environmental variable (e.g. *.vercel.app)
   if (process.env.VERCEL_URL)
     return(`https://${process.env.VERCEL_URL}${path}`)
   
+  // Otherwise, assume a local deployment of the server
   return (`http://localhost:${process.env.PORT ?? 3000}${path}`)
 }
