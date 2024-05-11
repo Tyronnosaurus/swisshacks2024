1d0fb4239390f65fe867bf4e80d90060c0db190d
Eduard Tiron <tyronnosaurus@protonmail.com>
2024-05-11 18:37:29 +0200

    Added tRPC route to create Stripe session

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -4,6 +4,9 @@ import { TRPCError } from '@trpc/server';
 import { db } from '@/db';
 import { z } from 'zod'
 import { INFINITE_QUERY_LIMIT } from '@/config/infinite-query';
+import { absoluteUrl } from '@/lib/utils';
+import { getUserSubscriptionPlan, stripe } from '@/lib/stripe';
+import { PLANS } from '@/config/stripe';
 
  
 // queries are for GET requests
@@ -55,6 +58,63 @@ export const appRouter = router({
         userId
       }
     })
+  }),
+
+
+  // Private procedure to create a Stripe session for the user to upgrade his plan.
+  // Returns link to Stripe checkout page.
+  createStripeSession: privateProcedure.mutation(async ({ctx}) => {   
+    
+    const billingUrl = absoluteUrl("/dashboard/billing")
+
+    // Get the user's info from the database
+    const {userId} = ctx
+
+    if(!userId) throw new TRPCError({code: "UNAUTHORIZED"})
+
+    const dbUser = await db.user.findFirst({
+      where:{
+        id: userId
+      }
+    })
+
+    if(!dbUser) throw new TRPCError({code: "UNAUTHORIZED"})
+
+
+    // Find out user's current subscription plan
+    const subscriptionPlan = await getUserSubscriptionPlan()
+    
+    // If user already has a paid subscription and a Stripe id in the database,
+    // just report this Id to Stripe (they already have the user's data)
+    if(subscriptionPlan.isSubscribed && dbUser.stripeCustomerId) {
+      const stripeSession = await stripe.billingPortal.sessions.create({
+        customer: dbUser.stripeCustomerId,
+        return_url: billingUrl
+      })
+
+      console.log("stripeSession for subscribed user with Stripe Id")
+      console.log(stripeSession)
+
+      return({url: stripeSession.url})
+    }
+
+    // Otherwise, create a session from scratch
+    const stripeSession = await stripe.checkout.sessions.create({
+      success_url: billingUrl,
+      cancel_url: billingUrl,
+      payment_method_types: ["card"],
+      mode: "subscription",
+      billing_address_collection: "auto",
+      line_items: [
+        {
+          price: PLANS.find((plan) => plan.name === "Pro")?.price.priceIds.test,
+          quantity: 1,
+        }
+      ],
+      metadata: {userId: userId}
+    })
+
+    return({url: stripeSession.url})
   }),
 
 
