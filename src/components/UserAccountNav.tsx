1bb489ee07dca087824b7dcecf09dfe90a3787e5
Eduard Tiron <tyronnosaurus@protonmail.com>
2024-05-18 15:07:20 +0200

    Added dropdown menu for navbar user icon

--- src/components/UserAccountNav.tsx
+++ src/components/UserAccountNav.tsx
@@ -1,11 +1,9 @@
 import { getUserSubscriptionPlan } from "@/lib/stripe"
-import { DropdownMenu, DropdownMenuTrigger } from "./ui/dropdown-menu"
+import { DropdownMenu, DropdownMenuTrigger, DropdownMenuContent } from "./ui/dropdown-menu"
 import { Button } from "./ui/button"
 import { Avatar, AvatarFallback } from "./ui/avatar"
 import Image from "next/image"
 import { Icons } from "./Icons"
-import { DropdownMenuContent } from "@radix-ui/react-dropdown-menu"
-
 
 interface UserAccountNavProps {
     email: string | undefined,
@@ -35,6 +33,15 @@ const UserAccountNav = async ({email, imageUrl, name}: UserAccountNavProps) => {
                 </Avatar>
             </Button>
         </DropdownMenuTrigger>
+
+        <DropdownMenuContent className="bg-white" align="end">
+            <div className="flex items-center justify-start gap-2 p-2">
+                <div className="flex flex-col space-y-0.5 leading-none">
+                    {name && <p className="font-medium text-sm text-black">{name}</p>}
+                    {email && <p className="w-[200px] truncate text-xs text-zinc-700">{email}</p>}
+                </div>
+            </div>
+        </DropdownMenuContent>
 
     </DropdownMenu>
   )
