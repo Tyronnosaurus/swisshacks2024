63000c2d31665a3f917b911c8331c83ff3cd0bf0
Eduard Tiron <tyronnosaurus@protonmail.com>
2024-05-29 21:57:53 +0200

    Enforce page limit for PDF uploads

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -6,6 +6,8 @@ import { PDFLoader } from "langchain/document_loaders/fs/pdf"
 import { PineconeStore } from "@langchain/pinecone";
 import { getPineconeClient } from "@/lib/pinecone";
 import { OpenAIEmbeddings } from "langchain/embeddings/openai"
+import { getUserSubscriptionPlan } from "@/lib/stripe";
+import { PLANS } from "@/config/stripe";
 
 
 const f = createUploadthing();
@@ -26,8 +28,10 @@ export const ourFileRouter = {
       // If no user is logged in, throw error and stop uploading to uploadThing
       if(!user || !user.id) throw new Error("Unauthorized")
        
+      const subscriptionPlan = await getUserSubscriptionPlan()
+
       // Whatever is returned here is accessible in onUploadComplete as `metadata`
-      return({userId: user.id})
+      return({subscriptionPlan, userId: user.id})
     })
 
     .onUploadComplete(async ({ metadata, file }) => {
@@ -54,7 +58,25 @@ export const ourFileRouter = {
 
         const pageLevelDocs = await loader.load()
 
+        // Enforce the limit on number of pages for free and paid users
         const pagesAmt = pageLevelDocs.length
+
+        const {subscriptionPlan} = metadata
+        const {isSubscribed} = subscriptionPlan
+
+        const isProExceeded = pagesAmt > PLANS.find((plan) => plan.name === "Pro")!.pagesPerPdf
+        const isFreeExceeded = pagesAmt > PLANS.find((plan) => plan.name === "Free")!.pagesPerPdf
+
+        if ((isSubscribed && isProExceeded) || (!isSubscribed && isFreeExceeded)){
+          await db.file.update({
+            data: {
+              uploadStatus: 'FAILED',
+            },
+            where: {
+              id: createdFile.id,
+            }
+          })
+        }
 
         // Vectorize and index the entire document
         const pinecone = getPineconeClient();
