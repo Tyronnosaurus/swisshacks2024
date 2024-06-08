a183ad93b0ae7e16b558c6509712c9850468afdd
Eduard Tiron <tyronnosaurus@protonmail.com>
2024-06-09 00:57:16 +0200

    PdfCards are now selectable

--- src/components/PdfCard.tsx
+++ src/components/PdfCard.tsx
@@ -2,18 +2,22 @@
 
 import { trpc } from "@/app/_trpc/client"
 import { Loader2, Trash } from "lucide-react"
-import Link from "next/link"
 import {format} from "date-fns"
 import { Button } from "./ui/button"
 import { useState } from "react"
 
 
 interface PdfCardProps {
-    file: {id: string, name: string, createdAt: string}
-  }
+  file: {id: string, name: string, createdAt: string},
+}
+
+
+const PdfCard = ({file}: PdfCardProps) => {
   
-  const PdfCard = ({file}: PdfCardProps) => {
-  
+    // State to manage checkbox selection
+    const [selected, setSelected] = useState<boolean>(false);
+
+
     // Id of the file currently being deleted, so that we can show feedback to the user (change trash icon to a loading icon)
     const [currentlyDeletingFile, setCurrentlyDeletingFile] = useState<string | null>(null)
   
@@ -29,22 +33,24 @@ interface PdfCardProps {
     })
   
     return(
-      <div className="col-span-1 divide-y divide-gray-200 rounded-lg bg-white shadow transition hover:shadow-lg">
-        <Link href={`/dashboard/${file.id}`} className="flex flex-col gap-2">
-          <div className="pt-6 px-6 flex w-full items-cneter justify-between space-x-6">
+      <div className={`col-span-1 divide-y divide-gray-200 rounded-lg bg-white shadow transition hover:shadow-lg border-emerald-600 ${selected ? "border-4" : ""}`}>
+        <div className="flex flex-col gap-2"
+             onClick={() => {setSelected(!selected)}}>
+            <div className="pt-6 px-6 flex w-full items-cneter justify-between space-x-6">
             
-            {/* Circle */}
-            <div className="h-10 w-10 flex-shrink-0 rounded-full bg-gradient-to-r from-cyan-500 to-blue-500" />
-            
-            {/* Filename */}
-            <div className="flex items-center justify-start space-x-3 flex-1 truncate">
-              <h3 className="truncate text-lg font-medium text-zinc-900">
-                {file.name}
-              </h3>
+                {/* Circle */}
+                <div className="h-10 w-10 flex-shrink-0 rounded-full bg-gradient-to-r from-cyan-500 to-blue-500" />
+                
+                {/* Filename */}
+                <div className="flex items-center justify-start space-x-3 flex-1 truncate">
+                    <h3 className="truncate text-lg font-medium text-zinc-900">
+                    {file.name}
+                    </h3>
+                </div>
+
             </div>
-  
-          </div>
-        </Link>
+        </div>
+
   
         <div className="px-6 mt-4 grid grid-cols-2 place-items-stretch py-2 gap-6 text-xs text-zinc-500">
           
@@ -64,6 +70,6 @@ interface PdfCardProps {
         </div>
       </div>
     )
-  }
+}
 
 export default PdfCard
