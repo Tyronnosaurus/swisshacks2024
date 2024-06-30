289ed9e7a5dda6de0ddee50add1298daf4330c16
Eduard Tiron <tyronnosaurus@protonmail.com>
2024-06-30 02:58:08 +0200

    Corrected aphostrophes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -100,9 +100,9 @@ export default function Home() {
               <li className="md:flex-1">
                 <div className="flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pl-0 md:pt-4">
                   <span className="text-sm font-medium text-blue-600">Step 2</span>
-                  <span className="text-xl font-semibold">Upload your PDF file</span>
+                  <span className="text-xl font-semibold">Upload your annual reports</span>
                   <span className="mt-2 text-zinc-700">
-                    We'll process your file and make it ready for you to chat with.
+                    We&apos;ll process your files and make them ready for you to interact with.
                   </span>
                 </div>
               </li>
@@ -112,7 +112,7 @@ export default function Home() {
                   <span className="text-sm font-medium text-blue-600">Step 3</span>
                   <span className="text-xl font-semibold">Start asking questions</span>
                   <span className="mt-2 text-zinc-700">
-                    It's that simple. Try out Quill today - it really takes less than a minute.
+                    It&apos;s that simple. Try it today - it really takes less than a minute.
                   </span>
                 </div>
               </li>
