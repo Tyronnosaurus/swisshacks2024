94306d4d25772cb69a37963b0c9dc97973730c65
Eduard Tiron <tyronnosaurus@protonmail.com>
2024-06-30 04:33:31 +0200

    Allow build errors

--- next.config.mjs
+++ next.config.mjs
@@ -26,9 +26,17 @@ const nextConfig = {
         return(config)
     },
 
-    images: {
-        domains: ["gravatar.com"],
-    }
+    // images: {
+    //     domains: ["gravatar.com"],
+    // }
+
+    typescript: {
+        // !! WARN !!
+        // Dangerously allow production builds to successfully complete even if
+        // your project has type errors.
+        // !! WARN !!
+        ignoreBuildErrors: true,
+      },
 };
 
 export default nextConfig;
